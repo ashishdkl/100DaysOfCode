c39dbbfa75397028b02c98d4b1c1e6ca18626f46
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-05-22 19:38:42 +0545

    bookmark

---
+++ JavaScript/Day 18 BookMark Manager/script.js
@@ -0,0 +1,5 @@
+let title = document.querySelector("#title");
+let url = document.querySelector("#url");
+let details = document.querySelector("#details");
+let type = document.querySelector("#type");
+let add = document.querySelector("#add");
