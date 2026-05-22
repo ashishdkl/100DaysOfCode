002fa6480b8eb9e9a22a7906e17bd122c9a436a4
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-05-22 21:34:25 +0545

    updated js

--- JavaScript/Day 18 BookMark Manager/script.js
+++ JavaScript/Day 18 BookMark Manager/script.js
@@ -3,7 +3,20 @@ let url = document.querySelector("#url");
 let details = document.querySelector("#details");
 let type = document.querySelector("#type");
 let add = document.querySelector("#add");
+let bookMark = [];
 
 add.addEventListener("click", () => {
-  console.log("hello");
+  event.preventDefault();
+  let titleSub = title.value;
+  let urlSub = url.value;
+  let detailsSub = details.value;
+  let typeSub = type.value;
+
+  let obj = {
+    title: titleSub,
+    url: urlSub,
+    details: detailsSub,
+    type: typeSub,
+  };
+  bookMark.push(obj);
 });
