75bc31f74ebdaa611c15d7602895a020729567b6
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-05-22 21:21:03 +0545

    js fixed

--- JavaScript/Day 18 BookMark Manager/script.js
+++ JavaScript/Day 18 BookMark Manager/script.js
@@ -3,3 +3,7 @@ let url = document.querySelector("#url");
 let details = document.querySelector("#details");
 let type = document.querySelector("#type");
 let add = document.querySelector("#add");
+
+add.addEventListener("click", () => {
+  console.log("hello");
+});
