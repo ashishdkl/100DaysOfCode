9ffbf4407e52617c1641c5275276b39d3a7e2139
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-05-23 22:09:50 +0545

    input event listner

--- JavaScript/Day 19 Callback and Typing test project/TypingTest/script.js
+++ JavaScript/Day 19 Callback and Typing test project/TypingTest/script.js
@@ -6,3 +6,14 @@ let bestScore = document.querySelector("#bestScore");
 let paragraph = document.querySelector("#paragraph");
 let textarea = document.querySelector("#textarea");
 let restart = document.querySelector("#restart");
+
+restart.addEventListener("click", () => {
+  textarea.value = "";
+  wpm.textContent = 0;
+  accuracy.textContent = 0;
+  mistake.textContent = 0;
+});
+
+textarea.addEventListener("input", () => {
+  console.log("Typing");
+});
