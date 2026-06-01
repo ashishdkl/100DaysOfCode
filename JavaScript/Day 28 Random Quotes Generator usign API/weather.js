5ab39c21c8d435046ece69817e5574af5717d857
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-06-01 22:29:32 +0545

    push

--- JavaScript/Day 28 Random Quotes Generator usign API/weather.js
+++ JavaScript/Day 28 Random Quotes Generator usign API/weather.js
@@ -1,5 +1,6 @@
 let input = document.querySelector("#input");
 let btn = document.querySelector("#btn");
+let display = document.querySelector("#display");
 
 btn.addEventListener("click", async () => {
   let location = input.value;
@@ -7,5 +8,9 @@ btn.addEventListener("click", async () => {
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=YOUR_API_KEY`,
   );
   let data = await response.json();
-  console.log(data);
+  display.innerHTML = `
+    <h3>${data.name}</h3>
+    <p>Temperature: ${data.main.temp}</p>
+    <p>Weather: ${data.weather[0].description}</p>
+  `;
 });
