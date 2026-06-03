e208b931ef0b59f33c2b816088760ade94f6fab6
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-06-03 09:14:20 +0545

    gpa calculator

--- JavaScript/Day 29 Random Quotes/GPA Calculator/script.js
+++ JavaScript/Day 29 Random Quotes/GPA Calculator/script.js
@@ -1,1 +1,10 @@
-console.log("Js");
+let courseAdd = document.querySelector("#course");
+let calculate = document.querySelector("#calculate");
+let sgpa = document.querySelector("#sgpa");
+courseAdd.addEventListener("click", () => {
+  console.log("Button Clicked");
+});
+
+calculate.addEventListener("click", () => {
+  console.log("calculate");
+});
