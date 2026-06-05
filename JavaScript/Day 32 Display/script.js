54d5dced1c8b1eeb9a5c9bffaf98a508c49afd14
ashishdkl <140738070+ashishdkl@users.noreply.github.com>
2026-06-05 22:41:24 +0545

    obj

--- JavaScript/Day 32 Display/script.js
+++ JavaScript/Day 32 Display/script.js
@@ -1,6 +1,12 @@
-let list=document.querySelector("#hello");
-let data=["nepal","ram","hari"];
+let list = document.querySelector("#hello");
+let data = ["nepal", "ram", "hari"];
 
-for(let i=0;i<data.length;i++){
-    list.innerHTML += `<li>${data[i]}</li>`;}
-    //using for loop
+for (let i = 0; i < data.length; i++) {
+  list.innerHTML += `<li>${data[i]}</li>`;
+}
+//using for loop
+
+let obj = {
+  name: Ashish,
+  age: 20,
+};
